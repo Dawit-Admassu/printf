ac6aebc1f391374fbf4ddd2e77fe4a19ab5e9762
Dawit.Admassu <daveadm1985@gmail.com>
2022-10-22 00:12:10 -0700

    11

--- get_print.c
+++ get_print.c
@@ -13,22 +13,11 @@
 int (*get_print(char s))(va_list, flags_t *)
 {
 	ph func_arr[] = {
-		{'i', print_int},
-		{'s', print_string},
-		{'c', print_char},
-		{'d', print_int},
-		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal},
-		{'R', print_rot13},
-		{'r', print_rev},
-		{'S', print_bigS},
-		{'p', print_address},
+		{'c', print_string},
+		{'s', print_char},
 		{'%', print_percent}
 	};
-	int flags = 14;
+	int flags = 3;
 
 	register int i;
 
